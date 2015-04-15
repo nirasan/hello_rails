e5664ec70ad424bd5557bfe517558ecc79dd22d3
nirasan <niranirao@gmail.com>
2015-04-15 16:52:16 +0900

    generate scaffold_controller Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs do
-    resources :entries
+    resources :entries do
+      resources :comments
+    end
   end
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
